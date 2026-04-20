eb0e2630d98634bd4c09c95301eb7437e1bbce6f
rvillatoro <rvilltdev@gmail.com>
2026-04-20 14:32:31 -0700

    Barebones page for WeightTracker added

--- frontend/simplestrength/src/WeightTracker.jsx
+++ frontend/simplestrength/src/WeightTracker.jsx
@@ -1,48 +1,76 @@
-import { useState } from "react";
+
+import { useState, useEffect } from "react";
 
 function WeightTracker() {
-  const [food, setFood] = useState("");
-  const [calories, setCalories] = useState("");
+  const [weight, setWeight] = useState("");
+  const [entries, setEntries] = useState([]);
 
-  const handleSubmit = async () => {
-    await fetch("http://localhost:3000/calories", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify({
-        food_name: food,
-        calories: calories
-      })
-    });
-
-    setFood("");
-    setCalories("");
+  // GET data
+  const fetchEntries = async () => {
+    try {
+      const res = await fetch("http://localhost:3000/weights");
+      const data = await res.json();
+      setEntries(data);
+    } catch(err) {
+      console.log("GET route not ready yet");
+    }
   };
+
+  useEffect(() => {
+    fetchEntries();
+  }, []);
+
+  // POST data 
+  const handleSubmit = async () => {
+    if (weight.trim() == "") {
+      alert("missing weight");
+      return;
+    }
+    try {
+      const res = await fetch("http://localhost:3000/weights", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          weight: Number(weight)
+        })
+      });
+
+      const result = await res.json();
+      console.log("POST result:", result);
+
+      await fetchEntries();
+    } catch (err) {
+      console.log("POST route not ready yet");
+    }
+
+    setWeight("");
+  }
 
   return (
     <div className="container">
-      <h2>Track weight</h2>
-
-      <input
-        type="text"
-        placeholder="Food name"
-        value={food}
-        onChange={(e) => setFood(e.target.value)}
-      />
+      <h2>Track Weight</h2>
 
       <input
         type="number"
-        placeholder="Calories"
-        value={calories}
-        onChange={(e) => setCalories(e.target.value)}
+        placeholder="Enter Weight"
+        value={weight}
+        onChange={(e) => setWeight(e.target.value)}
       />
 
       <button onClick={handleSubmit}>
         Add
       </button>
+
+      {entries.map((entry) => (
+        <p key={entry.id}>
+          {entry.weight}
+        </p>
+      ))}
     </div>
   );
 }
+
 
 export default WeightTracker;
